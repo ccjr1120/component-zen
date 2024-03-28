799e7a5252579dd213c91597867158f74e3879c3
匆匆孑然 <91577863+ccjr1120@users.noreply.github.com>
2024-03-28 19:20:02 +0800

    feat: stash

---
+++ packages/roll-component/vue-compiler/src/examples/Component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-container">
+    <div class="text">Roll-Component-Zen</div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped>
+.app-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  .text {
+    font-size: 22px;
+    font-weight: 660;
+  }
+}
+</style>
